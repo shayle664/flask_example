7a1bafdb6b693c827bb2fa2d326a9c9043bc0ed9
shayle <shayle@checkpoint.com>
2025-05-01 15:30:58 +0300

    chenge EOF

--- automation/deploy.sh
+++ automation/deploy.sh
@@ -24,21 +24,7 @@ function software_installation(){
 }
 
 function configure_nginx(){
-    tee /etc/nginx/sites-available/default > /dev/null << 'EOF'
-server {
-    listen 80;
-    server_name localhost;
-
-    location / {
-        proxy_pass http://127.0.0.1:5000;
-        proxy_set_header Host $host;
-        proxy_set_header X-Real-IP $remote_addr;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto $scheme;
-    }
-}
-EOF
-
+	cp default_site.tmpl /etc/nginx/sites-available/default
 	nginx -t && systemctl reload nginx
 }
 
